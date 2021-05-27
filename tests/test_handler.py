1819194539a27c2b409b6da84ade3c59f9882449
frank.gu <masami.nagasawa10@hotmail.com>
2021-05-27 14:22:13 +0800

    [ADD] (test) 增加了测试数据

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,7 +1,7 @@
 from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
 from utils.app import create_web_app
 import http
-
+import json
 
 class Test(AioHTTPTestCase):
     async def get_application(self):
@@ -20,23 +20,26 @@ class Test(AioHTTPTestCase):
     @unittest_run_loop
     async def test_post_curve_collectionhandle(self):
         # todo: mock data
-        mock_data = {
-            "result": [
-                {
-                    "name": "TimeMin_T",
-                    "i18n_name": "最小时间",
-                    "value": 0.0
-                },
-                {
-                    "name": "TimeMin_X",
-                    "i18n_name": "最小时间X",
-                    "value": 0.13123
-                }
-            ],
-            "curve": {
-                "force": [0.11, 0.12],
-                "time": [0.005, 0.01],
-                "distance": [0.2, 0.3]
-            }
-        }
+        # mock_data = {
+        #     "result": [
+        #         {
+        #             "name": "TimeMin_T",
+        #             "i18n_name": "最小时间",
+        #             "value": 0.0
+        #         },
+        #         {
+        #             "name": "TimeMin_X",
+        #             "i18n_name": "最小时间X",
+        #             "value": 0.13123
+        #         }
+        #     ],
+        #     "curve": {
+        #         "force": [0.11, 0.12],
+        #         "time": [0.005, 0.01],
+        #         "distance": [0.2, 0.3]
+        #     }
+        # }
+        mock_data = {}
+        with open("demo_data.json", 'r') as load_f:
+            mock_data = json.load(load_f)
         resp = await self.client.post("/curves", json=mock_data)
