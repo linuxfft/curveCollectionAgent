c74d802ea6ae12a9f81f5bb82894405931225187
sasaba <wang918562230@qq.com>
2021-05-28 10:19:27 +0800

    [IMP] (handler) 初步完善了解析数据的接口

--- utils/handler.py
+++ utils/handler.py
@@ -17,13 +17,13 @@ METHOD_DICT = {
 }
 
 
-async def doPushCurve2Airflow():
+async def doPushCurve2Airflow(data):
     async with aiohttp.ClientSession() as session:
         dt: Dict = METHOD_DICT.get('post_curve', {})
         mm = dt.get('method')
         m = getattr(session, mm)
         post_curve_url = parse.urljoin(ENV_AIRFLOW_BASE_URL, dt.get('url', ''))
-        async with m(post_curve_url) as resp:
+        async with m(post_curve_url, json=data) as resp:
             txt = await resp.text(encoding='utf-8')
             logger.info("推送曲线: {}, payload: {}".format(resp.status, txt))
             return txt
@@ -42,6 +42,46 @@ async def curveCollectionhandle(request):
     data: Dict = await request.json()
     result_content = data.get('result', '')
     curve_content = data.get('curve', '')
-    logger.debug("收到曲线数据: {}".format(pprint.pformat(data, indent=4)))
-    resp = await doPushCurve2Airflow()
+
+    measure_result = ""
+    entity_id = ""
+    for item_result in result_content:
+        if item_result.get("name") == "IsPass":
+            measure_result = "OK" if item_result.get("value") else "NOK"
+        if item_result.get("name") == "BarCade":
+            entity_id = item_result.get("value")
+
+    cur_m = curve_content.get("force")
+    cur_w = curve_content.get("distance")
+    cur_t = curve_content.get("time")
+    measure_torque = cur_m[-1]
+    measure_angle = cur_w[-1]
+    measure_time = cur_t[-1]
+
+    airflow_data = {
+      "replace_microseconds": 'false',
+      "conf": {
+        "entity_id": entity_id,
+        "result": {
+          "measure_result": measure_result,
+          "measure_torque": measure_torque,
+          "measure_angle": measure_angle,
+          "measure_time": measure_time,
+          "batch": '',
+          "count": 0,
+          "job": 1,
+          "controller_sn": "",
+          "controller_name": "CA-007R1",
+          "pset": 1,
+        },
+        "curve": {
+          "cur_m": measure_torque,
+          "cur_w": measure_angle,
+          "cur_t": measure_time
+        },
+      }
+    }
+
+    # logger.debug("收到曲线数据: {}".format(pprint.pformat(data, indent=4)))
+    resp = await doPushCurve2Airflow(airflow_data)
     return web.Response(text=resp)
